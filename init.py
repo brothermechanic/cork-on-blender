c361ee287846174b49c4213ac11a6e726dfb180d
neworigin2016 <ericxukai@gmail.com>
2017-11-02 10:31:16 -0400

    Update init.py to support execute from scripts
    Boolean operations can be both executed from invoke of press button and calling directly from scripts

--- init.py
+++ init.py
@@ -45,55 +45,116 @@ class CorkMeshSlicerPanel(Panel):
         layout = self.layout
 
         col = layout.column()
-        col.operator("view3d.cork_mesh_slicer", text="Union").method="UNION"
-        col.operator("view3d.cork_mesh_slicer", text="Difference").method="DIFF"
-        col.operator("view3d.cork_mesh_slicer", text="Intersect").method="INTERSECT"
-        col.operator("view3d.cork_mesh_slicer", text="XOR").method="XOR"
-        col.operator("view3d.cork_mesh_slicer", text="Resolve").method="RESOLVE"
+        col.operator("view3d.cork_mesh_union", text="Union")
+        col.operator("view3d.cork_mesh_difference", text="Difference")
+        col.operator("view3d.cork_mesh_intersect", text="Intersect")
+        col.operator("view3d.cork_mesh_xor", text="XOR")
+        col.operator("view3d.cork_mesh_resolve", text="Resolve")
         col.separator()
-        col.operator("view3d.cork_mesh_slicer", text="", icon='QUESTION', emboss=False).show_help = True
+        col.operator("view3d.cork_mesh_help", text="", icon='QUESTION', emboss=False).show_help = True
 
 
 # ############################################################
 # Operators
 # ############################################################
-
-class CorkMeshSlicerOperator(Operator):
+def check_errors(objects):
     """"""
-    bl_idname = "view3d.cork_mesh_slicer"
-    bl_label = "Mesh Slicer"
-    bl_description = ""
+    if len(objects) != 2:
+       raise NumberSelectionException
 
-    method = EnumProperty(
-        description="",
-        items=(("UNION", "Union", "A + B"),
-               ("DIFF", "Difference", "A - B"),
-               ("INTERSECT", "Intersection", "A n B"),
-               ("XOR", "XOR", "A xor B"),
-               ("RESOLVE", "Resolve", "Intersect and connect"),
-               ),
-        default="DIFF",
-        options={'SKIP_SAVE'},
-        )
+    for obj in objects:
+       if obj.type != 'MESH':
+          raise NonMeshSelectedException(obj)
 
-    show_help = bpy.props.BoolProperty(
-            name="Help",
-            description="",
-            default=False,
-            options={'HIDDEN', 'SKIP_SAVE'},
-            )
+def help_draw(_self, context):
+    layout = _self.layout
+    col = layout.column()
 
-    _commands = {
-            'UNION':'-union',
-            'DIFF':'-diff',
-            'INTERSECT':'-isct',
-            'XOR':'-xor',
-            'RESOLVE':'-resolve',
-            }
+    col.label(text="This operator works from the selected to the active objects")
+    col.label(text="The active must be a single plane")
 
-    @classmethod
-    def poll(cls, context):
-        return context.active_object and context.active_object.select
+    col.separator()
+    col.label(text="Union")
+    col.label(text="Compute the Boolean union of in0 and in1, and output the result")
+
+    col.separator()
+    col.label(text="Difference")
+    col.label(text="Compute the Boolean difference of in0 and in1, and output the result")
+
+    col.separator()
+    col.label(text="Intersect")
+    col.label(text="Compute the Boolean intersection of in0 and in1, and output the result")
+
+    col.separator()
+    col.label(text="XOR")
+    col.label(text="Compute the Boolean XOR of in0 and in1, and output the result")
+
+    col.separator()
+    col.label(text="Resolve")
+    col.label(text="Intersect the two meshes in0 and in1, and output the connected mesh with those")
+    col.label(text="intersections made explicit and connected")
+
+class CorkMeshBooleanUnion(Operator):
+    """Cork boolean union operation"""
+    bl_idname = "view3d.cork_mesh_union"  # NOTE: bl_idname should be lower case letters, no upper case letter allowed!! otherwise cause errors!!
+    bl_label = "cork boolean union"
+
+    def execute(self, context):
+        cork = get_cork_filepath(context)
+        try:
+            validate_executable(cork)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
+
+        try:
+            check_errors(context.selected_objects)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
+
+        self._cork = cork
+        self._plane = context.active_object
+        self._base = context.selected_objects[0] if context.selected_objects[0] != self._plane else context.selected_objects[1]
+        self._method = '-union'
+
+        return self.exec(context)
+    
+    def exec(self, context):
+        try:
+            slice_out(context, self._cork, self._method, self._base, self._plane)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
+
+        return {'FINISHED'}
+
+
+class CorkMeshBooleanDifference(Operator):
+    """Cork boolean difference operation"""
+    bl_idname = "view3d.cork_mesh_difference"
+    bl_label = "cork boolean difference"
+
+    def execute(self, context):
+        cork = get_cork_filepath(context)
+        try:
+            validate_executable(cork)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
+
+        try:
+            check_errors(context.selected_objects)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
+
+        self._cork = cork
+        self._plane = context.active_object
+        self._base = context.selected_objects[0] if context.selected_objects[0] != self._plane else context.selected_objects[1]
+        self._method = '-diff'
+
+        return self.exec(context)
 
     def exec(self, context):
         try:
@@ -104,13 +165,13 @@ class CorkMeshSlicerOperator(Operator):
 
         return {'FINISHED'}
 
-    def invoke(self, context, event):
-        if self.show_help:
-            context.window_manager.popup_menu(self.help_draw, title='Help', icon='QUESTION')
-            return {'CANCELLED'}
+class CorkMeshBooleanIntersect(Operator):
+    """Cork boolean intersect operation"""
+    bl_idname = "view3d.cork_mesh_intersect"
+    bl_label = "cork boolean intersect"
 
+    def execute(self, context):
         cork = get_cork_filepath(context)
-
         try:
             validate_executable(cork)
         except Exception as e:
@@ -118,7 +179,7 @@ class CorkMeshSlicerOperator(Operator):
             return {'CANCELLED'}
 
         try:
-            self.check_errors(context.selected_objects, self.method)
+            check_errors(context.selected_objects)
         except Exception as e:
             self.report({'ERROR'}, str(e))
             return {'CANCELLED'}
@@ -126,47 +187,105 @@ class CorkMeshSlicerOperator(Operator):
         self._cork = cork
         self._plane = context.active_object
         self._base = context.selected_objects[0] if context.selected_objects[0] != self._plane else context.selected_objects[1]
-        self._method = self._commands.get(self.method)
+        self._method = '-isct'
 
         return self.exec(context)
 
-    def check_errors(self, objects, method):
-        """"""
-        if len(objects) != 2:
-            raise NumberSelectionException
+    def exec(self, context):
+        try:
+            slice_out(context, self._cork, self._method, self._base, self._plane)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
 
-        for obj in objects:
-            if obj.type != 'MESH':
-                raise NonMeshSelectedException(obj)
+        return {'FINISHED'}
 
-    def help_draw(self, _self, context):
-        layout = _self.layout
-        col = layout.column()
+class CorkMeshBooleanXOR(Operator):
+    """Cork boolean XOR operation"""
+    bl_idname = "view3d.cork_mesh_xor"
+    bl_label = "cork boolean xor"
 
-        col.label(text="This operator works from the selected to the active objects")
-        col.label(text="The active must be a single plane")
+    def execute(self, context):
+        cork = get_cork_filepath(context)
+        try:
+            validate_executable(cork)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
 
-        col.separator()
-        col.label(text="Union")
-        col.label(text="Compute the Boolean union of in0 and in1, and output the result")
+        try:
+            check_errors(context.selected_objects)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
 
-        col.separator()
-        col.label(text="Difference")
-        col.label(text="Compute the Boolean difference of in0 and in1, and output the result")
+        self._cork = cork
+        self._plane = context.active_object
+        self._base = context.selected_objects[0] if context.selected_objects[0] != self._plane else context.selected_objects[1]
+        self._method = '-xor'
 
-        col.separator()
-        col.label(text="Intersect")
-        col.label(text="Compute the Boolean intersection of in0 and in1, and output the result")
+        return self.exec(context)
 
-        col.separator()
-        col.label(text="XOR")
-        col.label(text="Compute the Boolean XOR of in0 and in1, and output the result")
+    def exec(self, context):
+        try:
+            slice_out(context, self._cork, self._method, self._base, self._plane)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
 
-        col.separator()
-        col.label(text="Resolve")
-        col.label(text="Intersect the two meshes in0 and in1, and output the connected mesh with those")
-        col.label(text="intersections made explicit and connected")
+        return {'FINISHED'}
 
+class CorkMeshBooleanResolve(Operator):
+    """Cork boolean Resolve operation"""
+    bl_idname = "view3d.cork_mesh_resolve"
+    bl_label = "cork boolean Resolve"
+
+    def execute(self, context):
+        cork = get_cork_filepath(context)
+        try:
+            validate_executable(cork)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
+
+        try:
+            check_errors(context.selected_objects)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
+
+        self._cork = cork
+        self._plane = context.active_object
+        self._base = context.selected_objects[0] if context.selected_objects[0] != self._plane else context.selected_objects[1]
+        self._method = '-resolve'
+
+        return self.exec(context)
+
+    def exec(self, context):
+        try:
+            slice_out(context, self._cork, self._method, self._base, self._plane)
+        except Exception as e:
+            self.report({'ERROR'}, str(e))
+            return {'CANCELLED'}
+
+        return {'FINISHED'}
+
+class CorkMeshHelp(Operator):
+    """Cork boolean help operation"""
+    bl_idname = "view3d.cork_mesh_help"
+    bl_label = "cork boolean help"
+
+    show_help = bpy.props.BoolProperty(
+            name="Help",
+            description="",
+            default=False,
+            options={'HIDDEN', 'SKIP_SAVE'},
+            )
+
+    def execute(self, context):
+        if self.show_help:
+            context.window_manager.popup_menu(help_draw, title='Help', icon='QUESTION')
+            return {'CANCELLED'}
 
 # ############################################################
 # Registration
@@ -174,11 +293,21 @@ class CorkMeshSlicerOperator(Operator):
 
 def register():
     # the order here determines the UI order
-    bpy.utils.register_class(CorkMeshSlicerOperator)
+    bpy.utils.register_class(CorkMeshBooleanUnion)
+    bpy.utils.register_class(CorkMeshBooleanDifference)
+    bpy.utils.register_class(CorkMeshBooleanIntersect)
+    bpy.utils.register_class(CorkMeshBooleanXOR)
+    bpy.utils.register_class(CorkMeshBooleanResolve)
+    bpy.utils.register_class(CorkMeshHelp)
     bpy.utils.register_class(CorkMeshSlicerPanel)
 
 
 def unregister():
-    bpy.utils.unregister_class(CorkMeshSlicerPanel)
+    bpy.utils.unregister_class(CorkMeshBooleanUnion)
+    bpy.utils.unregister_class(CorkMeshBooleanDifference)
+    bpy.utils.unregister_class(CorkMeshBooleanIntersect)
+    bpy.utils.unregister_class(CorkMeshBooleanXOR)
+    bpy.utils.unregister_class(CorkMeshBooleanResolve)
+    bpy.utils.unregister_class(CorkMeshHelp)
     bpy.utils.unregister_class(CorkMeshSlicerOperator)
 
